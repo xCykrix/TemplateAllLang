3b4cf2180f9ee79e9fba39dd00a8ae22cc86a7c5
Samuel Voeller <12182390+xCykrix@users.noreply.github.com>
2024-04-19 21:05:51 +0000

    feat: update dev branch

--- dev/upstream_pull.sh
+++ dev/upstream_pull.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-echo "dev-script(): $1";
+echo "dev-script(): $1"
 
 # Apply consts and vars.
-TU_GIT="UPDATE_UPSTREAM_X1_LOCK";
-PWD=$(pwd);
-BN_PWD=$(basename $PWD);
+TU_GIT="UPDATE_UPSTREAM_X1_LOCK"
+PWD=$(pwd)
+BN_PWD=$(basename $PWD)
 
 # Abort if run in template to protect branch.
 if [[ "$BN_PWD" == "TemplateAllLang" ]]; then
-  echo "dev-script(): Unable to apply 'TemplateAllLang' to itself. Exiting...";
-  exit 1;
+  echo "dev-script(): Unable to apply 'TemplateAllLang' to itself. Exiting..."
+  exit 1
 fi
-echo "dev-script(): Applying Upstream 'TemplateAllLang' Update";
+echo "dev-script(): Applying Upstream 'TemplateAllLang' Update"
 
 # Delete failed sync attempt(s) and initialize template locally.
-rm -rf "./$TU_GIT";
-git clone --quiet https://github.com/xCykrix/TemplateAllLang "$TU_GIT" > /dev/null;
+rm -rf "./$TU_GIT"
+git clone --quiet https://github.com/xCykrix/TemplateAllLang "$TU_GIT" > /dev/null
 
 # Move Template Files
-cp -rf "./$TU_GIT/.github/" ".";
-cp -rf "./$TU_GIT/dev/" ".";
-cp -rf "./$TU_GIT/.editorconfig" ".";
-cp -rf "./$TU_GIT/.gitignore" ".";
-cp -rf "./$TU_GIT/LICENSE" ".";
-cp -rf "./$TU_GIT/make.sh" ".";
+cp -rf "./$TU_GIT/.github/" "."
+cp -rf "./$TU_GIT/dev/" "."
+cp -rf "./$TU_GIT/.editorconfig" "."
+cp -rf "./$TU_GIT/.gitignore" "."
+cp -rf "./$TU_GIT/LICENSE" "."
+cp -rf "./$TU_GIT/make.sh" "."
 
 # Delete Template
-rm -rf "./$TU_GIT";
+rm -rf "./$TU_GIT"
 
 exit 0
